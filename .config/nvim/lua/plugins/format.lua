a48b98f1be4ca2a3be24981644063bf6a08693a7
Tim Wundenberg <tim.wundenberg@breuninger.de>
2024-04-29 11:55:00 +0200

    update config

--- .config/nvim/lua/plugins/format.lua
+++ .config/nvim/lua/plugins/format.lua
@@ -5,21 +5,22 @@ return {
 		notify_on_error = true,
 		formatters_by_ft = {
 			svelte = { "prettier" },
+			vue = { "prettier" },
 		},
 		format_on_save = {
 			lsp_fallback = true,
-			timeout_ms = 500,
+			timeout_ms = 1500,
 		},
 	},
 	config = function(_, opts)
 		local plug = require('conform')
 		plug.setup(opts)
 
-		vim.api.nvim_create_autocmd("BufWritePre", {
-			pattern = "*",
-			callback = function(args)
-				plug.format({ bufnr = args.buf })
-			end,
-		})
+		-- vim.api.nvim_create_autocmd("BufWritePre", {
+		-- 	pattern = "*",
+		-- 	callback = function(args)
+		-- 		plug.format({ bufnr = args.buf })
+		-- 	end,
+		-- })
 	end,
 }
